e86f0c3beef8ba12dbc3b80b6f368747d9b760e6
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-01-16 10:22:29 +0530

    16-1

--- Day7_split_landingPage/index.js
+++ Day7_split_landingPage/index.js
@@ -9,7 +9,7 @@ const container = document.querySelector(".container")
 
 left.addEventListener("mouseenter", ()=> container.classList.add('hover-left'));
 
-left.addEventListener("mouserleave", ()=> container.classList.remove("hover-left"))
+left.addEventListener("mouseleave", ()=> container.classList.remove("hover-left"))
 
 
 right.addEventListener("mouseenter", ()=> container.classList.add("hover-right"));
