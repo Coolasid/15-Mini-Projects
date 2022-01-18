de1901f3d366859abefb9d652e851417307f87da
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-01-18 05:40:14 +0530

    18-1

---
+++ Day8_form_input_wave/index.js
@@ -0,0 +1,7 @@
+const lables = document.querySelectorAll(".formControl label")
+
+lables.forEach(lable =>{
+
+    lable.innerHTML = lable.innerText.split('').map((letter, idx)=>`<span style= "transition-delay: ${idx*50}ms"> ${letter}</span>`).join("");
+
+})
