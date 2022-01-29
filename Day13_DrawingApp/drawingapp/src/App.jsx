d873dc8238e2fd22d7753ec05258f409d65cdacf
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-01-29 06:49:46 +0530

    29-1

---
+++ Day13_DrawingApp/drawingapp/src/App.jsx
@@ -0,0 +1,138 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id='body'>
+
+      <canvas id='canvas' width="800" height="700"></canvas>
+
+      <div className='toolbox'>
+
+        <button id='decrease'>-</button>
+        <span id='size'>10</span>
+        <button id='increase'>+</button>
+        <input type="color" id='color'/>
+        <button id='clear'>X</button>
+
+      </div>
+     
+  
+     
+    </div>
+    
+  );
+}
+
+const canvas = document.getElementById("canvas");
+
+const incBtn = document.getElementById("increase");
+const decBtn = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
+
+
+const ctx = canvas.getContext('2d'); 
+
+let size = 10;
+let isPressed = false;
+colorEl.value = 'black'
+let color = colorEl.value;
+
+let x,y
+
+canvas.addEventListener("mousedown", (e)=>{
+
+  isPressed = true;
+
+  x = e.offsetX
+  y = e.offsetY
+
+
+})
+
+document.addEventListener("mouseup", (e)=>{
+
+  isPressed = false
+
+   x = undefined;
+   y = undefined;
+
+})
+
+canvas.addEventListener("mousemove", (e)=>{
+
+  if(isPressed){
+
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2)
+    drawLine(x, y, x2, y2)
+
+    x = x2;
+    y = y2;
+
+  }
+
+})
+
+function drawCircle(x, y){
+
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2)
+  ctx.fillStyle = color
+  ctx.fill()
+
+
+}
+
+function drawLine(x1, y1, x2, y2){
+
+  ctx.beginPath()
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color
+  ctx.lineWidth = size*2;
+  ctx.stroke();
+
+}
+
+function updateSizeOnScreen(){
+
+  sizeEl.innerText  = size;
+  
+
+}
+
+incBtn.addEventListener('click', ()=>{
+
+  size += 5;
+
+  if(size > 50){
+    size = 50
+  }
+
+  updateSizeOnScreen()
+})
+
+
+decBtn.addEventListener('click', ()=>{
+
+  size -= 5;
+
+  if(size < 5){
+    size = 5
+  }
+
+  updateSizeOnScreen()
+})
+
+colorEl.addEventListener("change", (e)=> color = e.target.value);
+
+clearEl.addEventListener("click", ()=> ctx.clearRect(0, 0, canvas.width, canvas.height))
+
+
+export default App;
+ 
