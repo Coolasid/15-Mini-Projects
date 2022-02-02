08f1c4c5e4663e08f3bfd45a07a53bd1c9a84141
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-02-02 06:01:14 +0530

    2-2

--- Day14_PepsiCard/pepsi_card/src/App.jsx
+++ Day14_PepsiCard/pepsi_card/src/App.jsx
@@ -1,12 +1,20 @@
 
 import './App.css';
-
+import  pepsiCan from './can-real-sugar-reg.png';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    < >
+        <div className='card'>
+          <div className='circle'></div>
+          <div className='content'>
+            <h2>Pepsi Cola</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex laudantium enim optio? Odit ipsum quo blanditiis repudiandae.</p>
+            <a href="#"> Buy Now</a>
+          </div>
+          <img src={pepsiCan} alt="pepsiCan"/>
+        </div>
+    </>
+  );        
 }
 
 export default App;
