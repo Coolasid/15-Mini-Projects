6dead2b27fdf699eefeed7770bba60d9517f09e6
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2022-02-19 17:12:10 +0530

    19-2

---
+++ Day15_SB_Coffee/starbucks-homepage/src/App.jsx
@@ -0,0 +1,19 @@
+import logo from './imgs/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <div id='navBar'>
+          <div>
+            <img src={logo} alt="" className='logo' />
+          </div>
+          <div>
+            
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
